0a4e90994a8fd2e5a73fa2a4d5ef963441f1fca6
Apollo3zehn <vwilms@outlook.com>
2021-02-18 14:36:24 +0100

    Add name parameter.

--- src/Extensions/PingV4.py
+++ src/Extensions/PingV4.py
@@ -6,10 +6,12 @@ from ..BaseTypes import Checker, CheckResult
 class PingV4Checker(Checker):
     Type: str = "ping-v4"
     Address: str
+    Name: str
 
     def __init__(self, settings: Dict[str, str]):
         super().__init__(settings)
         self.Address = settings["address"]
+        self.Name = settings["name"]
 
     def GetName(self) -> str:
         return f"Ping v4 ({self.Address})"
@@ -18,6 +20,6 @@ class PingV4Checker(Checker):
         success = not os.system('ping %s -n 1 > nul 2>&1' % (self.Address,))
 
         if success:
-            return self.Success()
+            return self.Success(self.Name)
         else:
-            return self.Error("Could not reach host.")
+            return self.Error(f"Could not reach host ({self.Name}).")
